03e420b3ef7d709184e02742792cb2b408c7b06f
bvjones <nanteos@hotmail.com>
2017-03-07 16:11:12 +0000

    add test for degrading quality 2x as fast once sell_in at 0

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -4,19 +4,24 @@ describe GildedRose do
 
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
+      items = [Item.new("Wallet", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+      expect(items[0].name).to eq "Wallet"
     end
     it 'item quality decreases over time' do
       items = [Item.new("Wallet", 10, 20)]
       GildedRose.new(items).update_quality()
-        expect(items[0].quality).to eq 19
+      expect(items[0].quality).to eq 19
     end
     it 'item sell_in decreases over time' do
       items = [Item.new("Wallet", 10, 20)]
       GildedRose.new(items).update_quality()
-        expect(items[0].sell_in).to eq 9
+      expect(items[0].sell_in).to eq 9
+    end
+    it 'once sell by passed, it degrades quality twice as fast' do
+      items = [Item.new("Wallet", 0, 20)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 18
     end
   end
 
