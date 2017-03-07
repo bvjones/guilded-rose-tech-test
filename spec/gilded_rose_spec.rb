a25150100098120d0d929ba4b380eec9cfe9e03e
bvjones <nanteos@hotmail.com>
2017-03-07 16:50:03 +0000

    add backstage pass test: quality will be 0 as sell in date is 0

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -69,6 +69,11 @@ describe GildedRose do
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq 5
       end
+      it 'quality will drop to 0 after sell_in date reaches 0' do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 2)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 0
+      end
     end
   end
 end
