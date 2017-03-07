2db33875e39cf006f8edd26a4842f2b8dca9dee9
bvjones <nanteos@hotmail.com>
2017-03-07 17:14:54 +0000

    add conjured item functionality

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -75,5 +75,12 @@ describe GildedRose do
         expect(items[0].quality).to eq 0
       end
     end
+    context 'Conjured' do
+      it 'quality degrades twice as fast as normal' do
+        items = [Item.new("Conjured", 10, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 8
+      end
+    end
   end
 end
